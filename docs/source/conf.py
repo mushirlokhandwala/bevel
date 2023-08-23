4b30b7c857732b75c2048e7871e808934ba64a98
mushirlokhandwala <mushirlokhandwala9@gmail.com>
2023-08-23 16:39:59 +0530

    immaterial theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -92,7 +92,7 @@ todo_include_todos = True
 #
 #html_theme = 'alabaster'
 #html_theme = 'sphinx_rtd_theme'
-html_theme = 'sphinx_material'
+html_theme = 'sphinx-immaterial'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
