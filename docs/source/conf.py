9de06948d83abc5e2eec028563ff8dab31c2bd77
mushirlokhandwala <mushirlokhandwala9@gmail.com>
2023-08-24 13:58:10 +0530

    pydata sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -93,15 +93,16 @@ todo_include_todos = True
 #
 #html_theme = 'alabaster'
 #html_theme = 'sphinx_rtd_theme'
-html_theme = 'sphinx_immaterial'
+#html_theme = 'sphinx_immaterial'
+html_theme = 'pydata_sphinx_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {
-    "palette": { "scheme": "default" }
-}
+# html_theme_options = {
+#     "palette": { "scheme": "default" }
+# }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
